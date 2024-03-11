cbee6162d30a9c03aa09ae0437b0eb4e101a2a57
Bravehearttttt <157325090+Bravehearttttt@users.noreply.github.com>
2024-03-11 13:05:11 -0500

    update

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from pathlib import Path
+
+path = Path("passwords/passwords.log")
 pwd = input("What is the master password")
+# def view():
+#     with open("password.txt","r") as file:
+
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    with open(path, "a") as file:
+        file.write(f"{name}|{pwd} ")
+
+
+while True:
+    mode = input(
+        "would you like to add new password or view existing ones(view,add or q to quit)?").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
